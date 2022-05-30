43cbe5c60cd2663c84e120e606302e5c5d4a81eb
Christian Camargo del moro <ccamargo@c1r4p1.42sp.org.br>
2022-05-30 17:14:02 -0300

    Inicial Commit

---
+++ ft_printf.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccamargo <ccamargo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 16:12:42 by ccamargo          #+#    #+#             */
+/*   Updated: 2022/05/30 17:11:25 by ccamargo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./includes/ft_printf.h"
+#include "./src/libft/libft.h"
+
+int    ft_printf(const char *, ...)
+{
+    size_t  count;
+
+    count = 0;
+    return (count);
+}
