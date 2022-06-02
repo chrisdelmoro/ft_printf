04d4c0a5dec370ac0594776a2d2ebba1a0b77556
ccamargo <ccamargo@student.42sp.org.br>
2022-06-02 19:38:30 -0300

    Changed folder structure

--- ft_printf.c
+++ ft_printf.c
@@ -3,20 +3,78 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ccamargo <ccamargo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ccamargo <ccamargo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 16:12:42 by ccamargo          #+#    #+#             */
-/*   Updated: 2022/05/30 17:11:25 by ccamargo         ###   ########.fr       */
+/*   Updated: 2022/06/02 19:35:58 by ccamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/ft_printf.h"
-#include "./src/libft/libft.h"
+#include "./libft/libft.h"
 
-int    ft_printf(const char *, ...)
+void print(const char *str, va_list args)
 {
-    size_t  count;
+	size_t	i;
 
-    count = 0;
-    return (count);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			if (str[i + 1] == 'c')
+			{
+				ft_putchar(va_arg(args, int));
+			}
+			if (str[i + 1] == 's')
+			{
+				ft_putstr(va_arg(args, char *));
+			}
+			if (str[i + 1] == 'p')
+			{
+				
+			}
+			if (str[i + 1] == 'd')
+			{
+				
+			}
+			if (str[i + 1] == 'i')
+			{
+				
+			}
+			if (str[i + 1] == 'u')
+			{
+				
+			}
+			if (str[i + 1] == 'x')
+			{
+				
+			}
+			if (str[i + 1] == 'X')
+			{
+				
+			}
+			if (str[i + 1] == '%')
+			{
+				ft_putchar('%');
+			}
+			i += 2;
+		}
+		else
+		{
+			ft_putchar(str[i]);
+			i++;
+		}
+	}
+}
+
+int ft_printf(const char *str, ...)
+{
+	va_list args;
+
+	va_start(args, str);
+	print(str, args);
+	va_end(args);
+	
+	return (1);
 }
