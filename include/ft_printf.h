97841faab9fba2ed6f7f5b5fe8e04a03bf285f1e
ccamargo <ccamargo@student.42sp.org.br>
2022-06-10 21:17:26 -0300

    Fixed hex function with negative numbers.

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ccamargo <ccamargo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 17:03:01 by ccamargo          #+#    #+#             */
-/*   Updated: 2022/06/10 20:12:49 by ccamargo         ###   ########.fr       */
+/*   Updated: 2022/06/10 21:16:25 by ccamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@
 
 int	ft_printf(const char *str, ...);
 int	ft_putunbr_fd(unsigned int n, int fd);
-size_t	ft_putnbr_hex_fd(long nbr, char *base, int fd);
+size_t	ft_putnbr_hex_fd(unsigned int nbr, char *base, int fd);
 
 #endif
--- src/ft_putnbr_hex_fd.c
+++ src/ft_putnbr_hex_fd.c
@@ -6,28 +6,19 @@
 /*   By: ccamargo <ccamargo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 14:39:35 by ccamargo          #+#    #+#             */
-/*   Updated: 2022/06/10 20:32:26 by ccamargo         ###   ########.fr       */
+/*   Updated: 2022/06/10 21:16:10 by ccamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_putnbr_hex_fd(long nbr, char *base, int fd)
+size_t	ft_putnbr_hex_fd(unsigned int nbr, char *base, int fd)
 {
-	unsigned long	ul_nbr;
-	size_t			count;
+	size_t	count;
 
 	count = 0;
-	if (nbr < 0)
-	{
-		ft_putchar_fd('-', fd);
-		count += 1;
-		ul_nbr = nbr * (-1);
-	}
-	else
-		ul_nbr = nbr;
-	if (ul_nbr >= 16)
-		count += ft_putnbr_hex_fd((ul_nbr / 16), base, fd);
-	ft_putchar_fd(base[ul_nbr % 16], fd);
+	if (nbr / 16 > 0)
+		count += ft_putnbr_hex_fd(nbr / 16, base, fd);
+	ft_putchar_fd(base[nbr % 16], fd);
 	return (count + 1);
 }
